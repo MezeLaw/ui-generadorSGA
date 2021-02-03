e80b87526114586ac1ff9acafe55dc4b2df32b3e
unknown <mezequielabogado@gmail.com>
2021-02-03 19:13:38 -0300

    Se agrega comportamiento de autoClose al presionar alguna opción y se cambia el icono dependiendo de si el sidenav esta abierto o no.

--- src/app/servicios/generador-sgaservice.service.ts
+++ src/app/servicios/generador-sgaservice.service.ts
@@ -9,17 +9,17 @@ import { Observable } from 'rxjs';
 })
 export class GeneradorSGAServiceService {
 
-  public localUrl = "http://localhost:8080/api"
-
+  public baseUrl = "http://localhost:8080/api"
+  //public baseUrl = "http://generadorsga.com.ar/api"
   constructor(public http : HttpClient) { }
 
   public getTags(){
-    return this.http.get<Elemento[]>(`${this.localUrl}/getTags`)
+    return this.http.get<Elemento[]>(`${this.baseUrl}/getTags`)
   }
 
 
   public downloadFile(fileName: string): Observable<any> {
-    return this.http.get(`${this.localUrl}/getTagByCode/${fileName}`, { responseType: 'blob' }).pipe(map((response : any)=>{
+    return this.http.get(`${this.baseUrl}/getTagByCode/${fileName}`, { responseType: 'blob' }).pipe(map((response : any)=>{
         return {
             filename: `${fileName}.pdf`,
             data: response
